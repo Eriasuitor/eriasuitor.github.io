bb6650412d5d63ae3ac9d666b36bb968f700fcc1
Lory <funtree9@icloud.com>
2023-06-25 14:32:51 +0800

    Home page has been finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,43 @@
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { getPosts, PostNode } from '../lib/post';
-import styles from '../styles/Home.module.css';
+import styles from '@/pages/index.module.css';
 import Title from '@/components/title';
+import _ from 'lodash';
+import classNames from 'classnames';
+import dayjs from 'dayjs';
 
-export default function Home({ posts }: { posts: PostNode[] }) {
+export default function Home({ posts }: { posts: [string, PostNode[]][] }) {
   return (
-    <div className={styles.container}>
-      <Title text='首页' />
-      <main>
-        <h1 className="title">
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-
-        <div className={styles.grid}>
-          <ul>
-            {posts.map(post => <li key={post.key} >
-              <Link href={`/posts/${post.key}`}>{post.title}</Link>
-            </li>)}
-          </ul>
-        </div>
-      </main>
-    </div>
+    <>
+      <Title text="首页" />
+      <div className={classNames([styles.main])}>
+        <ul>
+          {posts.map(([date, posts]) => <div key={date}>
+            <h1 className={classNames(styles.date_bar)}>{date}</h1>
+            {posts.map((post) => (
+              <li key={post.key} className={classNames(styles.post_line)}>
+                <Link href={`/posts/${post.key}`}>{post.title}</Link>
+              </li>
+            ))}
+          </div>
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = await getPosts();
-  return { props: { posts } };
+  return {
+    props: {
+      posts: _(posts)
+        .groupBy((p) => dayjs(p.date).format('YYYY'))
+        .mapValues((ps) => _.orderBy(ps, (p) => new Date(p.date ?? ''), 'desc'))
+        .toPairs()
+        .orderBy(([date]) => date, 'desc')
+        .value(),
+    },
+  };
 };
